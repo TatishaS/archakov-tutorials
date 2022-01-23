795e1859c6a96962e769d219e649597a4d617b58
Tati <snow_white_girl@bk.ru>
2022-01-23 18:38:58 +0300

    Homework #5 - phrases generator

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-/* import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import './tutorial-4/App.css';
+import App from './tutorial-4/App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-); */
+);
