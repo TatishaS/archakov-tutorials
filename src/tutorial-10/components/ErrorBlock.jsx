1d0a53d2ffab97ad2ce6afad40a6a4897841e4f7
Tati <snow_white_girl@bk.ru>
2022-02-09 13:21:07 +0300

    Homework #10 is ready - github profile search

---
+++ src/tutorial-10/components/ErrorBlock.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ErrorBlock = () => {
+  return (
+    <div className="app-user">
+      <div className="app-user_info">
+        <h2>Пользователь не найден</h2>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorBlock;
